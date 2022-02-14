870639469259b82c5fc600dd471766b70ee5cf44
Coder15YJ8965 <86280451+Coder15YJ8965@users.noreply.github.com>
2022-02-14 17:25:38 +0530

    Delete activity2.js

--- activity2.js
+++
@@ -1,9 +0,0 @@
-function back(){
-    window.location = "activity_1.html";
-}
-function getScore(){
-    score = localStorage.getItem("score");
-    document.getElementById("update").innerHTML="<h1>Score:" + score + "</h1>";
-}
-score = localStorage.getItem("score");
-document.getElementById("update").innerHTML = "<hh1>Score:" + score + "</h1>";
